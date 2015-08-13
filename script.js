395333c5757476ca171f591b6de456d60d235ec1
Dmitry Gempler <DGempler@gmail.com>
2015-08-13 09:11:55 -0700

    not getting close yet

--- script.js
+++ script.js
@@ -11,35 +11,59 @@
 
   var body = document.querySelector('body');
 
-  function rndClrNumGen() {
-    return Math.floor(Math.random() * 256);
+  var baseColor1 = 20;
+
+  var baseColor2 = 20;
+
+  var baseClrNumGen = function() {
+    return baseColor1 += 10;
+  };
+
+  var baseClrNumGen2 = function() {
+    return baseColor2 += 10;
+  };
+
+  var row1colors = [];
+  var row2colors = [];
+
+  for (var i = 0; i < 5; i++) {
+    row1colors[i] = baseClrNumGen();
   }
 
+  for (var i = 0; i < 4; i++) {
+    row2colors[i] = baseClrNumGen2();
+  }
 
-  function divCreatorBlack() {
+  function divCreatorBlack(rownumber) {
     var newDiv = document.createElement('div');
     newDiv.style.width = "11.1%";
     newDiv.style.paddingBottom = "11.1%";
     newDiv.style.cssFloat = "left";
-    newDiv.style.backgroundColor = "rgb(" + rndClrNumGen() + ",23,243)";
+    newDiv.style.backgroundColor = "rgb(" + rownumber + ",23,243)";
     return body.appendChild(newDiv);
   }
 
-  function divCreatorRed() {
+  function divCreatorRed(rownumber) {
     var newDiv2 = document.createElement('div');
     newDiv2.style.width = "11.1%";
     newDiv2.style.paddingBottom = "11.1%";
     newDiv2.style.cssFloat = "left";
-    newDiv2.style.backgroundColor = "rgb(" + rndClrNumGen() + ",242,20)";
+    newDiv2.style.backgroundColor = "rgb(" + rownumber + ",24,20)";
     return body.appendChild(newDiv2);
   }
 
-
-  for (var i = 0; i < 40; i++) {
-    divCreatorBlack();
-    divCreatorRed();
+  for (var k = 0; k < 5; k++) {
+    for (var j = 0; j < 4; j++) {
+      divCreatorBlack(row1colors[k]);
+      divCreatorRed(row2colors[k]);
+    }
+    divCreatorBlack(row1colors[k]);
+    for (var l = 1; l < 4; l++) {
+      divCreatorRed(row2colors[k]);
+      divCreatorBlack(row1colors[k]);
+    }
+    divCreatorRed(row2colors[k]);
   }
-  divCreatorBlack();
 
 
 
