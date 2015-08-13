ee4a6c309ee71777d86826734406fe17a81b60a1
Dmitry Gempler <DGempler@gmail.com>
2015-08-13 13:24:38 -0700

    Got it to work

--- script.js
+++ script.js
@@ -11,59 +11,44 @@
 
   var body = document.querySelector('body');
 
-  var baseColor1 = 20;
+  var baseColorOne = 20;
 
-  var baseColor2 = 20;
+  var baseColorTwo = 30;
 
-  var baseClrNumGen = function() {
-    return baseColor1 += 10;
+  var baseClrNumGen = function(row) {
+    return baseColorOne + (7 * row);
   };
 
-  var baseClrNumGen2 = function() {
-    return baseColor2 += 10;
+  var rowGenerator = function(rowNumber) {
+    var row = rowNumber;
+
+    return baseClrNumGen(row);
   };
 
-  var row1colors = [];
-  var row2colors = [];
 
-  for (var i = 0; i < 5; i++) {
-    row1colors[i] = baseClrNumGen();
-  }
-
-  for (var i = 0; i < 4; i++) {
-    row2colors[i] = baseClrNumGen2();
-  }
-
-  function divCreatorBlack(rownumber) {
+  function divCreatorOne(color) {
     var newDiv = document.createElement('div');
     newDiv.style.width = "11.1%";
     newDiv.style.paddingBottom = "11.1%";
     newDiv.style.cssFloat = "left";
-    newDiv.style.backgroundColor = "rgb(" + rownumber + ",23,243)";
+    newDiv.style.backgroundColor = "rgb(" + color + "," + color + ",243)";
     return body.appendChild(newDiv);
   }
 
-  function divCreatorRed(rownumber) {
+  function divCreatorTwo(color) {
     var newDiv2 = document.createElement('div');
     newDiv2.style.width = "11.1%";
     newDiv2.style.paddingBottom = "11.1%";
     newDiv2.style.cssFloat = "left";
-    newDiv2.style.backgroundColor = "rgb(" + rownumber + ",24,20)";
+    newDiv2.style.backgroundColor = "rgb(" + color + "," + color + ",20)";
     return body.appendChild(newDiv2);
   }
 
-  for (var k = 0; k < 5; k++) {
-    for (var j = 0; j < 4; j++) {
-      divCreatorBlack(row1colors[k]);
-      divCreatorRed(row2colors[k]);
+   for (var i = 0; i < 31; i++) {
+    divCreatorOne(baseClrNumGen(i));
+    divCreatorTwo(baseClrNumGen(i));
     }
-    divCreatorBlack(row1colors[k]);
-    for (var l = 1; l < 4; l++) {
-      divCreatorRed(row2colors[k]);
-      divCreatorBlack(row1colors[k]);
-    }
-    divCreatorRed(row2colors[k]);
-  }
+    divCreatorOne(baseClrNumGen(i));
 
 
 
